e2c61b5409f9e12d48a0dbbbba2d440b1addf11f
Ray Thurne Void <ray.thurne.void@gmail.com>
2021-05-18 14:47:53 +0200

    0.0.3: introdotto @raythurnevoid/node-colorful-log

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import { env } from "./env";
+import { Log } from "@raythurnevoid/node-colorful-log";
+
+const log = new Log("index.ts");
 
 async function main() {
-	console.log(`Hello ${env().test}`);
+	log.info(`Hello ${env().test}`);
 }
 
 main().catch((error) => {
